984088bee88f7c24da35b1a9d7f1cb1c2770e60e
francofallatti <francofallatti@gmail.com>
2024-05-04 14:01:35 -0300

    add init backend-offline

---
+++ Codigo/offline/api/app.py
@@ -0,0 +1,105 @@
+import os, json
+from flask import Flask, request, jsonify
+from models.User import db, User
+from models.PersonsImages import db, PersonsImages
+from db.db import init_db
+
+app = Flask(__name__)
+
+# Configurar la base de datos
+current_directory = os.path.dirname(os.path.realpath(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(current_directory, "db", "dataBase.db")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar la base de datos
+init_db(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message':'listening...'}),200
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = []
+    for user in users:
+        user_dict = {
+            'id': user.id,
+            'name': user.name,
+            'lastname': user.lastname
+        }
+        user_list.append(user_dict)
+    return jsonify(user_list)
+
+# Método DELETE para eliminar todos los usuarios solo para desarrolladores
+@app.route('/user', methods=['DELETE'])
+def delete_all_users():
+    try:
+        # Elimina todos los usuarios
+        num_deleted = db.session.query(User).delete()
+        db.session.commit()
+        
+        return jsonify({'message': f'{num_deleted} usuarios eliminados exitosamente'})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': 'Error al eliminar usuarios', 'details': str(e)}), 500
+
+
+# Método POST para agregar un usuario
+@app.route('/user', methods=['POST'])
+def add_user():
+    data = request.json
+    if 'name' not in data or 'lastname' not in data:
+        return jsonify({'error': 'Faltan campos en la solicitud'}), 400
+    
+    new_user = User(name=data['name'], lastname=data['lastname'])
+    db.session.add(new_user)
+    db.session.commit()
+    
+    # Consultar el usuario recién creado, comprobarlo y devolverlo
+    user = User.query.filter_by(id=new_user.id).first()
+    if user:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'lastname': user.lastname
+        }
+        return jsonify({'message': 'Datos almacenados exitosamente', 'user': user_data}), 201
+    else:
+        return jsonify({'error': 'No se pudo encontrar el usuario recién creado'}), 500
+
+# Metodo para guardar usuario en db
+@app.route('/insert_image', methods=['POST'])
+def insert_image():
+    try:
+        # Excepciones
+        if 'image' not in request.files:
+            return jsonify({'message': 'No se encontró ningún archivo en la solicitud.'}), 400
+
+        input_image = request.files['image']
+        if input_image.filename == '':
+            return jsonify({'message': 'El archivo no tiene nombre.'}), 400
+        
+        if not input_image.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            return jsonify({'message': 'El archivo no es una imagen válida.'}), 400
+        
+        # Insertar en db 
+        image_bytes = input_image.read()
+        image = PersonsImages(userId=1, photo=image_bytes)
+        db.session.add(image)
+        db.session.commit()
+
+
+        return jsonify({'message': 'Imagen insertada correctamente en la base de datos.'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Error al insertar la imagen en la base de datos.', 'error': str(e)}), 400
+
+
+def load_config(env):
+    with open(os.path.join(current_directory,'./config.json')) as f:
+        config = json.load(f)
+        return config.get(env, {})
+    
+if __name__ == '__main__':
+    config = load_config('development') # Carga los valores de 'development' 
+    app.run(host=config.get('host'), port=config.get('port'), debug=True)
