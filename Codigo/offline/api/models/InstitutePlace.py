ec84583fb7917a721cf2b3bc116deb4c11c686d3
francofallatti <francofallatti@gmail.com>
2024-05-04 17:16:08 -0300

    add institute-place

---
+++ Codigo/offline/api/models/InstitutePlace.py
@@ -0,0 +1,11 @@
+from db.db import db
+
+# Modelo para la tabla intermedia 'InstitutePlace'
+class InstitutePlace(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    institute_id = db.Column(db.Integer, db.ForeignKey('institute.id'))
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'))
+
+    # Relaciones con las tablas Institute y Place
+    institute = db.relationship('Institute', backref=db.backref('institute_places', cascade='all, delete-orphan'))
+    place = db.relationship('Place', backref=db.backref('place_institutes', cascade='all, delete-orphan'))
