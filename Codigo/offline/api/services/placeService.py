ca7ee118d11ca6cf2e36d488f833fd91bd7120f5
fran <66625402+francofallatti@users.noreply.github.com>
2024-05-08 21:42:52 -0300

    add get place by id and fix createDate

--- Codigo/offline/api/services/placeService.py
+++ Codigo/offline/api/services/placeService.py
@@ -1,9 +1,10 @@
 from db.db import db
 from models.Place import Place
+from utils.date import createDate
 
 def savePlace(data):
     try:
-        place = Place(name=data.get('name'), abreviation=data.get('abreviation'), description=data.get('description'), time=data.get('time'))
+        place = Place(name=data.get('name'), abbreviation=data.get('abbreviation'), description=data.get('description'), openTime=data.get('openTime'), closeTime=data.get('closeTime'), createDate=createDate())
         db.session.add(place)
         db.session.commit()
         
@@ -19,11 +20,27 @@ def updatePlace(id, data):
     
     try:
         place.name = data.get('name')
-        place.abreviation = data.get('abreviation')
+        place.abbreviation = data.get('abbreviation')
         place.description = data.get('description')
-        place.time = data.get('time')
+        place.openTime=data.get('openTime') 
+        place.closeTime=data.get('closeTime')
         db.session.commit()
 
         return 200
     except Exception as e:
         return e
+
+def getPlaceById(id):
+    place = Place.query.get(id)
+    if place:
+        return {
+            'id': place.id,
+            'name': place.name,
+            'abbreviation': place.abbreviation,
+            'description': place.description,
+            'openTime': place.openTime,
+            'closeTime': place.closeTime,
+            'createDate': place.createDate
+        }
+    else:
+        return None
