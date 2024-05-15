5a0c4f8801c9ec7b9d09df34dfef83cb90f5e485
David <daviddanielc25@gmail.com>
2024-05-15 12:39:31 -0300

    Añadimos algunos modelos

--- Codigo/frontend/api/model/interfaces.ts
+++ Codigo/frontend/api/model/interfaces.ts
@@ -12,3 +12,10 @@ interface visitante{
     places: string[],
     category: string,
 }
+
+// types.ts
+export interface Categoria {
+    id?: number; // id es opcional porque podría no estar presente en la carga útil de la solicitud
+    nombre: string;
+    lugares: string[];
+}
