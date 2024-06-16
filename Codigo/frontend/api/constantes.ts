8f8b434fa99853063b721bd4c6b99e026ed1d67c
fran <66625402+francofallatti@users.noreply.github.com>
2024-06-16 00:27:10 -0300

    delete old frontend

--- Codigo/frontend/api/constantes.ts
+++
@@ -1,3 +0,0 @@
-const IP = "localhost"
-const PUERTO = 3000
-export const URL =  `http://${IP}:${PUERTO}/`;
--- Codigo/frontend/util/parserFechaToSring.ts
+++
@@ -1,3 +0,0 @@
-export function fechaToString(fecha: Date) : string {
-    return fecha.getDate() + "/" + fecha.getMonth() + "/" + fecha.getFullYear();
-}
