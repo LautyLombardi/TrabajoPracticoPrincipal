6e752fbe0e039e6480bfce4ae8d13730721fafde
fran <66625402+francofallatti@users.noreply.github.com>
2024-06-21 14:17:01 -0300

    base de datos productiva

--- Codigo/online/api/app.py
+++ Codigo/online/api/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 from db.db import init_db
 from controllers import *
 from flask_cors import CORS
-from db.Populate import populate_places, populate_institutes,populate_institute_places
 
 app = Flask(__name__)
 
@@ -16,7 +15,6 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(current_directory, "db", "dataBase.db")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 init_db(app)
-
 
 @app.route('/', methods=['GET'])
 def index():
