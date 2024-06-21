01333254cc69b8b4cdcb5f44d62719483075f4dc
Facundo Hernandez <facujhernandez46@gmail.com>
2024-06-20 22:03:15 -0300

    add egreso in login manual and face recognition login

--- Codigo/frontend/hooks/logs/useInsertLoginLog.tsx
+++ Codigo/frontend/hooks/logs/useInsertLoginLog.tsx
@@ -2,6 +2,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useSQLiteContext } from '@/context/SQLiteContext';
 import { getCurrentCreateDate } from '@/util/getCreateDate';
 import { useCallback } from 'react';
+import { Logs } from '@/api/model/interfaces';
 
 const useInsertLoginLog = () => {
     const db = useSQLiteContext();
@@ -9,48 +10,46 @@ const useInsertLoginLog = () => {
     const insertLoginLog = useCallback(async (admDni: number, dniCliente: number, table_login: string) => {
         const createDate = getCurrentCreateDate();
         console.log('data a cargar', admDni, dniCliente, table_login);
-        if(table_login.toLowerCase()=="visitante") {
-            try {
-                await db.execAsync('BEGIN TRANSACTION;');
-                let description="se logeo manualmente un "+table_login
-                const result = await db.runAsync(
-                    `INSERT INTO logs (admDni, visitorId, description, createDate, isEnter) VALUES (?, ?, ?, ?, 1);`,
-                    [admDni, dniCliente, description, createDate]
-                );
+        let description = `se logeo manualmente un ${table_login}`;
 
-                console.log('Insert result:', result.lastInsertRowId, result.changes);
-                const logId = result.lastInsertRowId;
+        const getLatestLogQuery = (table: string) => `
+            SELECT * FROM logs
+            WHERE ${table}Id = ?
+            ORDER BY createDate DESC
+            LIMIT 1;
+        `;
 
-                await db.execAsync('COMMIT;');
+        try {
+            await db.execAsync('BEGIN TRANSACTION;');
+            
+            const tableId = table_login.toLowerCase() === "visitante" ? "visitor" : "user";
+            const latestLog = await db.getFirstAsync<Logs>(getLatestLogQuery(tableId), [dniCliente]);
 
-                console.log('log inserted with ID:', logId);
-                return logId;
-            } catch (error) {
-                await db.execAsync('ROLLBACK;');
-                console.error('Error inserting log:', error);
-                return 0;
+            let isEnter = 1;
+            if (latestLog) {
+                const latestLogDate = new Date(latestLog.createDate).toDateString();
+                const currentDate = new Date(createDate).toDateString();
+                
+                if (latestLogDate === currentDate && latestLog.isEnter === 1) {
+                    isEnter = 0;
+                }
             }
-        }else{
-            try {
-                await db.execAsync('BEGIN TRANSACTION;');
-                let description="se logeo manualmente un "+table_login
-                const result = await db.runAsync(
-                    `INSERT INTO logs (admDni, userId, description, createDate, isEnter) VALUES (?, ?, ?, ?, 1);`,
-                    [admDni, dniCliente, description, createDate]
-                );
 
-                console.log('Insert result:', result.lastInsertRowId, result.changes);
-                const logId = result.lastInsertRowId;
+            const result = await db.runAsync(
+                `INSERT INTO logs (admDni, ${tableId}Id, description, createDate, isEnter) VALUES (?, ?, ?, ?, ?);`,
+                [admDni, dniCliente, description, createDate, isEnter]
+            );
 
-                await db.execAsync('COMMIT;');
+            console.log('Insert result:', result.lastInsertRowId, result.changes);
+            const logId = result.lastInsertRowId;
 
-                console.log('Log inserted with ID:', logId);
-                return logId;
-            } catch (error) {
-                await db.execAsync('ROLLBACK;');
-                console.error('Error inserting Log:', error);
-                return 0;
-            }
+            await db.execAsync('COMMIT;');
+            console.log('Log inserted with ID:', logId);
+            return logId;
+        } catch (error) {
+            await db.execAsync('ROLLBACK;');
+            console.error('Error inserting log:', error);
+            return 0;
         }
     }, [db]);
 
