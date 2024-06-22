38c5fe1237e583a87093a98d27861902394b5f46
fran <66625402+francofallatti@users.noreply.github.com>
2024-06-22 16:38:39 -0300

    update syncController

--- Codigo/online/api/controllers/syncController.py
+++ Codigo/online/api/controllers/syncController.py
@@ -2,154 +2,68 @@ from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 from services.syncService import *
 
-sync_bp = Blueprint("sync",__name__)
+sync_bp = Blueprint("sync", __name__)
 CORS(sync_bp)
 
 @sync_bp.route('/logs', methods=['POST'])
 def sync_logs():
     logs = request.json
-    errores = []
+    return handle_sync_response(syncLogs, logs)
 
-    for log in logs:
-        response = syncLogs(log)
-        if response is not True:
-            errores.append({'data': log, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync logs exitoso'}), 201
-
-@sync_bp.route('/users',methods=['POST'])
+@sync_bp.route('/users', methods=['POST'])
 def create_users():
     users = request.json
-    errores = [] 
-
-    for user in users:
-        response = syncUser(user)
-        if response is not True:
-            errores.append({'data': user, 'error': response})
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync users exitoso'}), 201
+    return handle_sync_response(syncUser, users)
     
 @sync_bp.route('/visitors', methods=['POST'])
 def sync_visitors():
     visitors = request.json
-    errores = []
-
-    for visitor in visitors:
-        response = syncVisitors(visitor)
-        if response is not True:
-            errores.append({'data': visitor, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync visitors exitoso'}), 201
+    return handle_sync_response(syncVisitors, visitors)
 
 @sync_bp.route('/places', methods=['POST'])
 def sync_places():
     places = request.json
-    errores = []
-
-    for place in places:
-        response = syncPlaces(place)
-        if response is not True:
-            errores.append({'data': place, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync places exitoso'}), 201
+    return handle_sync_response(syncPlaces, places)
 
 @sync_bp.route('/categories', methods=['POST'])
 def sync_categories():
     categories = request.json
-    errores = []
+    return handle_sync_response(syncCategories, categories)
 
-    for category in categories:
-        response = syncCategories(category)
-        if response is not True:
-            errores.append({'data': category, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync categories exitoso'}), 201
-    
 @sync_bp.route('/exceptions', methods=['POST'])
 def sync_exceptions():
     exceptions = request.json
-    errores = []
-
-    for exception in exceptions:
-        response = syncExceptions(exception)
-        if response is not True:
-            errores.append({'data': exception, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync exceptions exitoso'}), 201
+    return handle_sync_response(syncExceptions, exceptions)
     
 @sync_bp.route('/enterprices', methods=['POST'])
 def sync_enterprices():
     enterprices = request.json
-    errores = []
-
-    for enterprice in enterprices:
-        response = syncEnterprices(enterprice)
-        if response is not True:
-            errores.append({'data': enterprice, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync enterprices exitoso'}), 201
+    return handle_sync_response(syncEnterprices, enterprices)
 
 @sync_bp.route('/institutes', methods=['POST'])
 def sync_institutes():
     institutes = request.json
-    errores = []
-
-    for institute in institutes:
-        response = syncInstitutes(institute)
-        if response is not True:
-            errores.append({'data': institute, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync institutes exitoso'}), 201
+    return handle_sync_response(syncInstitutes, institutes)
 
 @sync_bp.route('/user-histories', methods=['POST'])
 def sync_user_histories():
     user_histories = request.json
-    errores = []
-
-    for user_history in user_histories:
-        response = syncUserHistories(user_history)
-        if response is not True:
-            errores.append({'data': user_history, 'error': response})
-
-    if errores:
-        return jsonify({'errores': errores}), 400
-    else:
-        return jsonify({'message': 'Sync user histories exitoso'}), 201
+    return handle_sync_response(syncUserHistories, user_histories)
 
 @sync_bp.route('/visitor-histories', methods=['POST'])
 def sync_visitor_histories():
     visitor_histories = request.json
+    return handle_sync_response(syncVisitorHistories, visitor_histories)
+    
+def handle_sync_response(sync_function, data_list):
     errores = []
 
-    for visitor_history in visitor_histories:
-        response = syncVisitorHistories(visitor_history)
+    for data in data_list:
+        response = sync_function(data)
         if response is not True:
-            errores.append({'data': visitor_history, 'error': response})
+            errores.append({'data': data, 'error': response})
 
     if errores:
         return jsonify({'errores': errores}), 400
     else:
-        return jsonify({'message': 'Sync visitor histories exitoso'}), 201
+        return jsonify({'message': f'Sync {sync_function.__name__} exitoso'}), 201
