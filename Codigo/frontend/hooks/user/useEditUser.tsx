2e9dc9fc02156634339b9d08869f6e5ae82361c5
Facundo Hernandez <facujhernandez46@gmail.com>
2024-06-24 22:42:21 -0300

    fixed userEdit

--- Codigo/frontend/hooks/user/useEditUser.tsx
+++ Codigo/frontend/hooks/user/useEditUser.tsx
@@ -1,29 +1,27 @@
 import { useSQLiteContext } from '@/context/SQLiteContext';
 import { getCurrentCreateDate } from '@/util/getCreateDate';
 import { useCallback } from 'react';
-import { parseFecha } from '@/util/parseDate';
 import { Usuario } from '@/api/model/interfaces';
 
 const useEditUser = () => {
     const db = useSQLiteContext();
 
-    const editUser = useCallback(async (userOld: Usuario, dniNew: number, nameNew: string, lastnameNew: string, role_idNew: number | undefined, passwordNew: string, dateActive: string, motive: string) => {
+    const editUser = useCallback(async (userOld: Usuario, dniNew: number, nameNew: string, lastnameNew: string, role_idNew: number | undefined, passwordNew: string, motive: string) => {
         const createDate = getCurrentCreateDate();
-        const dateActiveN = parseFecha(dateActive);
-        console.log('Data a modificar', userOld, dniNew, nameNew, lastnameNew, role_idNew, 'passwordNew', passwordNew, dateActiveN, motive);
+        console.log('Data a modificar', userOld, dniNew, nameNew, lastnameNew, role_idNew, 'passwordNew', passwordNew, motive);
         try {
             await db.execAsync('BEGIN TRANSACTION;');
             
             // Insert into user_history
             await db.runAsync(
                 `INSERT INTO user_history (dniO, dniN, role_idO, role_idN, nameO, nameN, lastnameO, lastnameN, passwordO, passwordN, isActiveO, isActiveN,
-                                        motiveO, motiveN, activeDateO, activeDateN, createDateO, createDateN ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, '', ?, 0, 1, '', ?, '', ?, ?, ?);`,
-                [userOld.dni, dniNew, userOld.role_id, role_idNew || userOld.role_id, userOld.name, nameNew, userOld.lastname, lastnameNew, passwordNew || '', motive, dateActiveN, userOld.createDate,createDate]
+                                        motiveO, motiveN, activeDateO, createDateO, createDateN ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, '', ?, 0, 1, '', ?, ?, ?, ?);`,
+                [userOld.dni, dniNew, userOld.role_id, role_idNew || userOld.role_id, userOld.name, nameNew, userOld.lastname, lastnameNew, passwordNew || '', motive, userOld.activeData || null, userOld.createDate,createDate]
             )
 
             // Update user table
-            const updateParams = [dniNew, role_idNew || userOld.role_id, nameNew, lastnameNew, dateActiveN, createDate, userOld.dni];
-            let updateQuery = `UPDATE user SET dni = ?, role_id = ?, name = ?, lastname = ?, activeDate = ?, createDate = ?`;
+            const updateParams = [dniNew, role_idNew || userOld.role_id, nameNew, lastnameNew, createDate, userOld.dni];
+            let updateQuery = `UPDATE user SET dni = ?, role_id = ?, name = ?, lastname = ?, createDate = ?`;
 
             if (passwordNew) {
                 updateQuery += `, password = ?`;
